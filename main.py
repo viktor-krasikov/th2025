e2b6614a86f3d8c14fa16eef9941a76fc7d2fd23
Viktor Krasikov <viktor.krasikov@gmail.com>
2025-03-01 13:41:25 +0800

    Добавил список ИНН

--- main.py
+++ main.py
@@ -36,15 +36,43 @@ def get_unique_inns():
     # Преобразуем результат в формат JSON
     return unique_inns.to_dict(orient='records')
 
+
+@app.get("/inns")
+def get_inns(q: str = Query(None)):
+    # Извлечение необходимых колонок
+    suppliers = df[['ИНН победителя КС', 'Наименование победителя КС']].drop_duplicates()
+
+    # Фильтрация по параметру q, если он задан
+    if q:
+        suppliers = suppliers[
+            suppliers['ИНН победителя КС'].astype(str).str.contains(q, case=False) |
+            suppliers['Наименование победителя КС'].str.contains(q, case=False)
+            ]
+
+    # Переименование колонок
+    suppliers = suppliers.rename(columns={
+        'ИНН победителя КС': 'inn',
+        'Наименование победителя КС': 'name'
+    })
+
+    # Сортировка по колонке 'inn'
+    suppliers = suppliers.sort_values(by='inn')
+
+    # Преобразование в список словарей
+    suppliers_list = suppliers.to_dict(orient='records')
+
+    return JSONResponse(content=suppliers_list)
+
+
 @app.get("/top_customers_plot.jpg")
-async def get_top_customers_plot():
-    # Топ-10 заказчиков по количеству КС
-    top_customers = df['Наименование заказчика'].value_counts().nlargest(10)
+async def get_top_customers_plot(count: int = Query(10, ge=1)):
+    # Топ-N заказчиков по количеству КС
+    top_customers = df['Наименование заказчика'].value_counts().nlargest(count)
 
     # Создание графика
     plt.figure(figsize=(10, 6))
     top_customers.plot(kind='bar', color='skyblue')
-    plt.title('Топ-10 заказчиков по количеству КС', fontsize=16)
+    plt.title(f'Топ-{count} заказчиков по количеству КС', fontsize=16)
     plt.xlabel('Заказчики', fontsize=14)
     plt.ylabel('Количество КС', fontsize=14)
     plt.xticks(rotation=45, ha='right')
@@ -58,15 +86,16 @@ async def get_top_customers_plot():
 
     return StreamingResponse(buf, media_type="image/jpeg")
 
+
 @app.get("/top_suppliers_plot.jpg")
-async def get_top_suppliers_plot():
-    # Топ-10 поставщиков по общей сумме выигранных КС
-    top_suppliers = df.groupby('Наименование победителя КС')['Конечная цена КС (победителя в КС)'].sum().nlargest(10)
+async def get_top_suppliers_plot(count: int = Query(10, ge=1)):
+    # Топ-N поставщиков по общей сумме выигранных КС
+    top_suppliers = df.groupby('Наименование победителя КС')['Конечная цена КС (победителя в КС)'].sum().nlargest(count)
 
     # Создание графика
     plt.figure(figsize=(10, 6))
     top_suppliers.plot(kind='bar', color='lightgreen')
-    plt.title('Топ-10 поставщиков по общей сумме выигранных КС', fontsize=16)
+    plt.title(f'Топ-{count} поставщиков по общей сумме выигранных КС', fontsize=16)
     plt.xlabel('Поставщики', fontsize=14)
     plt.ylabel('Сумма выигранных КС', fontsize=14)
     plt.xticks(rotation=45, ha='right')
